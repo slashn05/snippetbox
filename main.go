c055dd7512fe9d0bcd9a43491bb27b655f6fe1d4
Sandeep Edara - s0e01w9 <Sandeep.Edara@walmartlabs.com>
2023-12-23 11:29:46 +0530

    Added new routes to the servemux

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is the snippet view method"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is the snippet create method"))
+}
+
+func snippetRoot(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is the root of the snippet"))
+}
 
 func main() {
 	fmt.Println("Hello World!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", snippetRoot)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Print("starting server on 4000 port")
+
+	err := http.ListenAndServe(":4000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
